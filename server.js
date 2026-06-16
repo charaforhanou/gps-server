89f4898c00fc4fde43db5ad814ed487390fc9987
charaf_orhanou <133925996+charaforhanou@users.noreply.github.com>
2026-06-16 15:41:29 +0100

    update server

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.get('/position', (req, res) => {
   res.json(lastPosition);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Serveur GPS sur port ${PORT}`));
